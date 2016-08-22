307dea48ae2f7b8a5681af1a4a61e4696acde611
Louis <louis.northmore@gmail.com>
2016-08-22 19:38:47 +0100

    Livestreamer & launcher shell script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+echo "Installing Livestreamer"
+sudo pip install livestreamer
