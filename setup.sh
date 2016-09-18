de485bf998482d02a672156ccac98cad7646fc2d
Louis Northmore <louis.northmore@gmail.com>
2016-09-18 07:19:24 +0100

    Add python via brew for pip

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
+brew install python
 echo "Installing Livestreamer"
 sudo pip install livestreamer
