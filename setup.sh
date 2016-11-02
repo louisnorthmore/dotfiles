772b7a86e09596d033fc098e42f026edabecf697
Louis Northmore <louis.northmore@gmail.com>
2016-11-02 08:40:16 +0000

    Remove brew

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
-brew install python
-echo "Installing Livestreamer"
-sudo pip install livestreamer
+#brew install python
+#echo "Installing Livestreamer"
+#sudo pip install livestreamer
